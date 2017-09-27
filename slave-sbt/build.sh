50d4f3ec58c3f81d252134868c2e11194d91e323
Robbie Blaine <robbie@zenaptix.com>
2017-09-27 10:19:34 +0200

    imagestream in slave-sbt build.sh

--- slave-sbt/build.sh
+++ slave-sbt/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t docker-registry-default.zenshift.io/jenkins/jenkins-sbt .
 docker push docker-registry-default.zenshift.io/jenkins/jenkins-sbt
-oc label jenkins-sbt role=jenkins-slave
-oc annotate jenkins-sbt role=jenkins-slave
+oc label imagestream jenkins-sbt role=jenkins-slave
+oc annotate imagestream jenkins-sbt role=jenkins-slave
